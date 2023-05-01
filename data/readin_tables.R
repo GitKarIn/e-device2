d300eac6be2525a79a5075be5f6228039f2bcde1
gitKarIn <Karrin.Alstad@wildlife.ca.gov>
2023-05-01 15:02:53 -0700

    add data folder and read program

---
+++ data/readin_tables.R
@@ -0,0 +1,16 @@
+library(readxl)  
+
+folder = "C:/Users/KAlstad/Kar_Docs/ProjectMangmt/e-Devices/DeviceComparisons"
+
+read_excel_allsheets <- function(filename, tibble = FALSE) {
+  # I prefer straight data.frames
+  # but if you like tidyverse tibbles (the default with read_excel)
+  # then just pass tibble = TRUE
+  sheets <- readxl::excel_sheets(filename)
+  x <- lapply(sheets, function(X) readxl::read_excel(filename, sheet = X))
+  if(!tibble) x <- lapply(x, as.data.frame)
+  names(x) <- sheets
+  x
+}
+
+survey123 <- read_excel_allsheets(file.path(folder,"Survey123_app_criteria.xlsx"))
