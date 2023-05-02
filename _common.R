ec875cd0340b7af7ea5e9fa65b8c8baa52262923
gitKarIn <Karrin.Alstad@wildlife.ca.gov>
2023-05-02 08:45:19 -0700

    adding excel read function to the _common.R file so that it will be compiled with regular build

--- _common.R
+++ _common.R
@@ -6,3 +6,16 @@ knitr::opts_chunk$set(
   comment = "#>",
   collapse = TRUE
   )
+
+Xdatafld = "C:/Users/KAlstad/Kar_Docs/ProjectMangmt/e-Devices/DeviceComparisons"
+
+read_excel_allsheets <- function(filename, tibble = FALSE) {
+  # I prefer straight data.frames
+  # but if you like tidyverse tibbles (the default with read_excel)
+  # then just pass tibble = TRUE
+  sheets <- readxl::excel_sheets(filename)
+  x <- lapply(sheets, function(X) readxl::read_excel(filename, sheet = X))
+  if(!tibble) x <- lapply(x, as.data.frame)
+  names(x) <- sheets
+  x
+}
