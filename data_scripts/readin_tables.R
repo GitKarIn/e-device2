ffd7a4cc5b2923b8c5ace4942202e0aa14623216
gitKarIn <Karrin.Alstad@wildlife.ca.gov>
2023-05-03 12:52:47 -0700

    Add appendices section and e-device survey processing code to readin_tables.R

--- data_scripts/readin_tables.R
+++ data_scripts/readin_tables.R
@@ -10,3 +10,52 @@ read_excel_allsheets <- function(filename, tibble = FALSE) {
 Xdatfldr = "C:/Users/KAlstad/Kar_Docs/ProjectMangmt/e-Devices/DeviceComparisons"
 survey123 <- read_excel_allsheets(file.path(Xdatfldr,"Survey123_app_criteria.xlsx"))
 survey123$Forms_Options$Feature = ifelse(is.na(survey123$Forms_Options$Feature),"",survey123$Forms_Options$Feature)
+
+## e-device survey results
+
+library(readxl)
+library(dplyr)
+
+setwd("C:/Users/KAlstad/Kar_Docs/ProjectMangmt/e-Devices/SurveyResults/")
+Survey <- read_excel("Survey1.xlsx",sheet = "Sheet1")
+
+Survey1 <- Survey %>% 
+  select(-`Completion time`,-Email,-"Contact Email (optional)",-"Contact Phone (optional)",-Name) %>% 
+  rename(name="Contact name",Agency="Contact Agency/Affiliation")
+
+Survey3 <- Survey1 %>%
+  rename( 
+    dev_exp=5,
+    exp_status=6,
+    field_app = 7,
+    what_hard  = 8,
+    weather_case =9,
+    what_soft = 10,
+    yr_reasons = 11,
+    you_dem =12,
+    type_data = 13,
+    dev_consid =14,
+    key_challenges =15,
+    type_data_nohope =16,
+    whathaveyoutried =17,
+    reas_nodevice =18,
+    hrdwr_feachz = 19,
+    sftwr_feachz = 20,
+    other_feachz = 21,
+    extern_contact = 22)
+
+Survey3$Agency[grepl("CDFW",Survey3$Agency)]<-"CDFW"
+Survey3$Agency[grepl("USFWS",Survey3$Agency)]<-"USFWS"
+Survey3$Agency[grepl("Water Resources",Survey3$Agency)]<-"DWR"
+Survey3$Agency[grepl("Fish and Wildlife",Survey3$Agency)]<-"CDFW"
+Survey3$Agency[grepl("UC",Survey3$Agency)]<-"UC"
+Survey3$Agency
+
+
+Survey3$exp_status[grepl("I have previously used",Survey3$exp_status)]<-"I have previously used"
+Survey3$exp_status[grepl("I am currently using",Survey3$exp_status)]<-"I am currently using"
+
+## Save to .rs file ##
+
+saveRDS(Survey3, file = "Survey3.rds")
+
